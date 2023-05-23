171f87fbf5ca0fd69acc90946622c4870753fda3
Craig Gooden <craig.gooden@capgemini.com>
2023-05-23 12:41:18 +0100

    A BMI Calculator

---
+++ bmi_calculator.py
@@ -0,0 +1,7 @@
+#This is part of the day 2 challenges
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+new_height = float(height)
+new_weight = int(weight)
+total = (new_weight / (new_height ** 2))
+print(total)
