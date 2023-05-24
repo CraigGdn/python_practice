d3e9da3da21a563b528e0e2bd50a6d9e8db819b9
Craig Gooden <craig.gooden@capgemini.com>
2023-05-24 10:29:37 +0100

    Days Months and Weeks till 90

---
+++ age_days_calculator.py
@@ -0,0 +1,10 @@
+age = input("What is your current age? ")
+
+#90 * 365 = 32850
+#90 * 52 = 4680
+#90 * 1080
+x = (90 - int(age)) * 365 
+y = (90 - int(age)) * 52
+z = (90 - int(age)) * 12
+
+print(f"You have {x} days, {y} weeks, and {z} months left.")
